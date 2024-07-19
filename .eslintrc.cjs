2145cafc558b54db45233cf444f05a0e9dd86bb1
Matheus <matheusramosbi.13@gmail.com>
2024-07-19 10:58:37 -0300

    first commit, ready to start

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
+    'no-unused-vars': 'off',
+    'react/jsx-key': 'off',
   },
-}
+};
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div>teste</div>;
+}
+
+export default App;
